0e3392e07758c8379e7e4bd17cad962f33590d26
Алёна <alena@MacBook-Pro-Pavel-2.local>
2022-06-10 18:43:52 +0300

    added logo

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './styles/main.css'
+
+const Main = () => {
+    return (
+      <div className="main">
+        <div id="canvas" className="magical"></div>
+      </div>
+    );
+}
+
+export default Main
